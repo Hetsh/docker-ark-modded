0415997ffae02c37a888196712f1068f987020a6
Hetsh <git@hetsh.de>
2020-09-30 11:12:18 +0200

    Added CKF mods to updates

--- update.sh
+++ update.sh
@@ -21,7 +21,9 @@ update_image "hetsh/ark" "ARK" "true" "\d+-\d+"
 
 # Mods
 update_mod "731604991" "Structures Plus" "SP_VER"
-update_mod "558651608" "Bridge" "BRIDGE_VER"
+update_mod "558651608" "Bridges" "BRIDGE_VER"
+update_mod "1814953878" "Castles, Keeps and Forts: Remastered" "CKFR_VER"
+update_mod "2121156303" "Castles, Keeps and Forts: Science Fiction" "CKFSF_VER"
 
 if ! updates_available; then
 	#echo "No updates available."
